6afaf48d5acc21b0af44d63de0dc351a17a90dc2
Gabriel P. Bezerra <gabriel.bezerra@ufv.br>
2021-03-10 10:44:54 -0300

    Adiciona execução de migração de banco durante inicialização da aplicação

--- src/main/java/com/github/gabrielpbzr/cookbook/Application.java
+++ src/main/java/com/github/gabrielpbzr/cookbook/Application.java
@@ -1,10 +1,12 @@
 package com.github.gabrielpbzr.cookbook;
 
+import com.github.gabrielpbzr.cookbook.exceptions.DatabaseMigrationException;
 import com.github.gabrielpbzr.cookbook.web.RecipeController;
 import com.github.gabrielpbzr.cookbook.recipes.RecipeDAO;
 import com.github.gabrielpbzr.cookbook.recipes.RecipeDAOJPA;
 import com.github.gabrielpbzr.cookbook.utils.Configuration;
 import com.github.gabrielpbzr.cookbook.utils.EntityManagerProvider;
+import com.github.gabrielpbzr.cookbook.utils.Migrations;
 import io.javalin.Javalin;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -17,6 +19,8 @@ import static io.javalin.apibuilder.ApiBuilder.*;
  * @author gabriel
  */
 public final class Application {
+
+    private final Configuration config;
     private final Logger logger;
     private final EntityManagerProvider entityManagerProvider;
     private Javalin javalinApp;
@@ -24,22 +28,32 @@ public final class Application {
     public Application(@NotNull Configuration config) {
         this.logger = LoggerFactory.getLogger(config.getValue("app.name"));
         this.entityManagerProvider = new EntityManagerProvider(config);
+        this.config = config;
     }
 
     private void init() {
         logger.debug("Initializing app...");
-        RecipeDAO recipeDao = new RecipeDAOJPA(entityManagerProvider.getEntityManager());
-        RecipeController recipeController = new RecipeController(recipeDao, logger);
-        this.javalinApp = Javalin.create().routes(() -> {
-            crud("/api/recipes/:id", recipeController);
-        });
-        logger.debug("App initialized!");
+        try {
+            
+            Migrations.apply(config);
+            logger.debug("Database migrations applied...");
+            
+            RecipeDAO recipeDao = new RecipeDAOJPA(entityManagerProvider.getEntityManager());
+            RecipeController recipeController = new RecipeController(recipeDao, logger);
+            
+            this.javalinApp = Javalin.create()
+                    .routes(() -> crud("/api/recipes/:id", recipeController));
+            
+            logger.debug("App initialized!");
+        } catch (DatabaseMigrationException me) {
+            logger.error("Error during database migration", me);
+        }
     }
 
     public void start(int port) {
         this.init();
         javalinApp.start(port);
-        logger.debug("Application started on port "+ port);
+        logger.debug("Application started on port " + port);
     }
 
     public void stop() {
@@ -47,7 +61,7 @@ public final class Application {
         javalinApp.stop();
         logger.info("Application stopped");
     }
-    
+
     public String getBaseURL() {
         return String.format("http://localhost:%d", javalinApp.port());
     }
