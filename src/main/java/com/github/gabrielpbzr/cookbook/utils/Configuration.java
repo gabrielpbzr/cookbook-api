0346f974b4a4676faaac9d77ad26544ab2bd9e08
Gabriel P. Bezerra <gabriel.bezerra@ufv.br>
2021-03-10 14:15:20 -0300

    Altera a classe Configuration para carregar os valores do ambiente

--- src/main/java/com/github/gabrielpbzr/cookbook/utils/Configuration.java
+++ src/main/java/com/github/gabrielpbzr/cookbook/utils/Configuration.java
@@ -1,23 +1,9 @@
 package com.github.gabrielpbzr.cookbook.utils;
 
-import java.io.IOException;
-import java.util.Properties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Configuration {
-    private static final String APP_CONFIG_FILE = "config.properties";
     private static Configuration instance;
-    private Properties properties;
-    private final Logger logger = LoggerFactory.getLogger(Configuration.class);
 
-    private Configuration() {
-        try {
-            this.properties = new Properties();
-            properties.load(getClass().getClassLoader().getResourceAsStream(APP_CONFIG_FILE));
-        } catch (IOException ioe) {
-            logger.error("Configuration file not found.", ioe);
-        }
+    private Configuration() {        
     }
 
     public static Configuration getInstance() {
@@ -28,6 +14,6 @@ public class Configuration {
     }
 
     public String getValue(String key) {
-        return properties.getProperty(key, "");
+        return (System.getenv(key) != null)  ? System.getenv(key) : "";
     }
 }
